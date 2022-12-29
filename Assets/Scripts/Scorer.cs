3cfaa91482d029cf6f5f7d2c0880dceaec8266b0
Armando Interaminense <ar.interaminense@gmail.com>
2022-12-29 12:33:31 -0500

    Scorer script
    Scorer script implemented and function to count numbers of bumps into a wall

---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int score = 0;
+    private void OnCollisionEnter(Collision collision)
+    {
+        score++;
+        Debug.Log("You've bumped into a thing this many times: " + score);
+    }
+}
