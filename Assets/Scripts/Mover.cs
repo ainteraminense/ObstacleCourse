fe083e2fa457484f131e60196858c305459c8c92
Armando Interaminense <ar.interaminense@gmail.com>
2022-12-29 11:33:13 -0500

    Added PrintInstructions and MovePlayer
    Create PrintInstructions() and MovePlayer() methods

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -9,13 +9,26 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        PrintInstructions();
     }
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Use AWSD or arrow keys to move the player");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);
     }
+
 }
