799da2aa76e394c3186bd749b01aaa4c3e69c52e
Armando Interaminense <ar.interaminense@gmail.com>
2022-12-29 15:15:02 -0500

    Finish designing the level

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -8,7 +8,7 @@ public class ObjectHit : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            GetComponent<MeshRenderer>().material.color = Color.cyan;
+            GetComponent<MeshRenderer>().material.color = Color.red;
             tag = "Hit";
         }   
     }
